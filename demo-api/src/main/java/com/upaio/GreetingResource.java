5967e271c4ebf11ea5d22894f2dca42d70b9fee0
Jesus Aguirre <info.aguirre.jesus@gmail.com>
2022-10-22 11:18:31 -0500

    Agregar configuracion inicial de pruebas unitarias automaticas, junto con el manejo dinamico niveles de trazas de log

--- demo-api/src/main/java/com/upaio/GreetingResource.java
+++ demo-api/src/main/java/com/upaio/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.upaio;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,13 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.debug("Hi from debug");
+        LOG.info("Hi from info");
         return "Hello from RESTEasy Reactive";
     }
 }
---
+++ demo-api/src/main/java/com/upaio/LoggingResource.java
@@ -0,0 +1,34 @@
+package com.upaio;
+
+import javax.ws.rs.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Path("/logging")
+public class LoggingResource {
+
+    private static Level getLogLevel(Logger logger) {
+        for (Logger current = logger; current != null;) {
+            Level level = current.getLevel();
+            if (level != null)
+                return level;
+            current = current.getParent();
+        }
+        return Level.INFO;
+    }
+
+    @GET
+    @Path("/{logger}")
+    @Produces("text/plain")
+    public Level logger(@PathParam("logger") String loggerName, @QueryParam("level") String level) {
+        // Get the logger instance
+        Logger logger = Logger.getLogger(loggerName);
+
+        // Change the log-level if requested
+        if (level != null && level.length() > 0)
+            logger.setLevel(Level.parse(level));
+
+        // Return the current log-level
+        return getLogLevel(logger);
+    }
+}
